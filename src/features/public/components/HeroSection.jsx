30ac1599ea359aedaad4ba2f0af132da987f4b49
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-05-15 20:37:06 +0630

    public-page-optimize

--- src/features/public/components/HeroSection.jsx
+++ src/features/public/components/HeroSection.jsx
@@ -5,9 +5,16 @@ const HeroSection = () => {
     <section className="hero bg-white dark:bg-gray-200 min-h-screen">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold text-gray-800 dark:text-white">Hello there</h1>
+          <h1 className="text-5xl font-bold text-gray-800 max-sm:text-2xl dark:text-white">
+            Voucher app Dashboard
+          </h1>
           <p className="py-6">
-            MMSIt မှ Laravel ဖြင့် ဖန်တီးထားသော aip ကို react js ဖြင့်ရေးထားသော dashboard ဖြစ်သည်။
+            MMSIt မှ Laravel ဖြင့် ဖန်တီးထားသော aip ကို react js ဖြင့်ရေးထားသော
+            dashboard ဖြစ်သည်။
+          </p>
+          <p>
+            <span>Email - admin@gmail.com</span>/{" "}
+            <span>Password - asdffdsa</span> ဖြင့် login ဝင်ပါ။
           </p>
         </div>
       </div>
